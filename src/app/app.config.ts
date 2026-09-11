9618c5250e65d4df230c0ddd96c36d6dec5b7fb1
sseva <severfam@mail.ru>
2026-09-11 16:22:36 +0400

    fix: replace info icon with ℹ️

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,7 +16,7 @@ export const appConfig: ApplicationConfig = {
         icons: {
           success: '\u{1F44D}',
           error: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"/></svg>',
-          info: '\u{1310}\uFE0F',
+          info: '\u2139\uFE0F',
           close: '\u2716',
         },
       },
